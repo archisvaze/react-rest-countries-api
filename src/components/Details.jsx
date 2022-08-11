cf69e1d21267a0a5ccffa4b0313734c8b2071e36
archis <vazearchis@gmail.com>
2022-08-11 06:22:38 +0530

    added media querries

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -90,19 +90,23 @@ export default function Details() {
                     <div className="details-info">
                         <h2>{commonName}</h2>
 
-                        <div className="details-main-info">
-                            <p className="d-info">Official Name : <span className='d-info-value'>{officialName}</span></p>
-                            <p className="d-info">Population : <span className='d-info-value'>{population}</span></p>
-                            <p className="d-info">Region : <span className='d-info-value'>{region}</span></p>
-                            <p className="d-info">Sub Region : <span className='d-info-value'>{subregion}</span></p>
-                            <p className="d-info">Capital : <span className='d-info-value'>{capital}</span></p>
+                        <div className="details-inner-info">
+                            <div className="details-main-info">
+                                <p className="d-info">Official Name : <span className='d-info-value'>{officialName}</span></p>
+                                <p className="d-info">Population : <span className='d-info-value'>{population}</span></p>
+                                <p className="d-info">Region : <span className='d-info-value'>{region}</span></p>
+                                <p className="d-info">Sub Region : <span className='d-info-value'>{subregion}</span></p>
+                                <p className="d-info">Capital : <span className='d-info-value'>{capital}</span></p>
+                            </div>
+
+                            <div className="details-sub-info">
+                                <p className="d-info">Timezone : <span className='d-info-value'>{timezone}</span></p>
+                                <p className="d-info">Currencies : <span className='d-info-value'>{currencies}</span></p>
+                                <p className="d-info">Languages : <span className='d-info-value'>{languages}</span></p>
+                            </div>
                         </div>
 
-                        <div className="details-sub-info">
-                            <p className="d-info">Timezone : <span className='d-info-value'>{timezone}</span></p>
-                            <p className="d-info">Currencies : <span className='d-info-value'>{currencies}</span></p>
-                            <p className="d-info">Languages : <span className='d-info-value'>{languages}</span></p>
-                        </div>
+
                     </div>
 
                 </div>
